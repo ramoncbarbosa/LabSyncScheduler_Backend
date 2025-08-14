792644b1cd0919f85d47ce7efd70320a15fa34f4
ramoncbarbosa <ramonsengineer.oficial@gmail.com>
2025-08-14 01:44:01 -0300

    feat(initial-setup): Initial project setup for the LabSync Scheduler

---
+++ labschedulerg-backend-reactive/src/main/java/com/magoramon/labschedulerg_backend_reactive/LabschedulergBackendReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.magoramon.labschedulerg_backend_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabschedulergBackendReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabschedulergBackendReactiveApplication.class, args);
+	}
+
+}
---
+++ labschedulerg-backend-reactive/src/test/java/com/magoramon/labschedulerg_backend_reactive/LabschedulergBackendReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magoramon.labschedulerg_backend_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabschedulergBackendReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
